3b059a9b2e6ea2b0d5a08f6310997fefb00e7355
panqingcui <panqingcui@eeefung.com>
2019-09-12 13:45:30 +0800

    [Modify] docker file

---
+++ demo2/README.md
@@ -0,0 +1,7 @@
+### 本文用于 spring boot 2.0 与 docker 整合实例 
+spring boot docker example
+### 项目环境
+- jdk 8
+- spring boot 2.1.8.RELEASE
+- maven 3.6*
+### 
---
+++ demo2/src/main/java/com/example/demo/controller/DemoController.java
@@ -0,0 +1,24 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * <p>功能描述,该部分必须以中文句号结尾 .</p>
+ * <p>
+ * 创建日期 2019/9/10
+ *
+ * @author panqingcui
+ * @since 1.0.0
+ */
+@Controller
+public class DemoController {
+    @RequestMapping(value = "/",method = RequestMethod.GET)
+    @ResponseBody
+    public String index(){
+        System.out.println("--------------------------->>>>>>**************");
+        return "this is docker spring boot";
+    }
+}
